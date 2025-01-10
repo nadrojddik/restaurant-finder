250cc9a5f7d743ef130ef851103476181258c25e
nadrojddik <mail@jordankidd.com>
2025-01-10 01:22:35 -0500

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/restaurant-finder/', // This should match your repository name
+  base: '/',  // Changed from '/restaurant-finder/' to '/'
 })
