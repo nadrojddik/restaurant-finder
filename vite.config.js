1c18828b6b8fa61a1f316778900f68e75819d178
nadrojddik <mail@jordankidd.com>
2025-01-10 19:39:18 -0500

    updates

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/restaurant-finder/',  // This is crucial for GitHub Pages
   plugins: [react()],
-  base: '/',  // Changed from '/restaurant-finder/' to '/'
 })
