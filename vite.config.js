1a757a4c19f87ed586c6a7a7cd41aa8c5fa6412a
nadrojddik <mail@jordankidd.com>
2025-01-10 21:54:11 -0500

    massive cleanup

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/restaurant-finder/', // Must match your repository name
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
-  },
-  define: {
-    'process.env.VITE_GOOGLE_MAPS_API_KEY': JSON.stringify(process.env.VITE_GOOGLE_MAPS_API_KEY)
-  }
+  base: '/restaurant-finder/',
+  plugins: [react()]
 })
