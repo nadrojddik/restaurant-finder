4d32f046cba32fbf5dc7218d3a33caf55bc55886
nadrojddik <mail@jordankidd.com>
2025-01-10 19:43:59 -0500

    js path bugs

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/restaurant-finder/',  // This is crucial for GitHub Pages
+  base: '/restaurant-finder/', // Must match your repository name
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
+  define: {
+    'process.env.VITE_GOOGLE_MAPS_API_KEY': JSON.stringify(process.env.VITE_GOOGLE_MAPS_API_KEY)
+  }
 })
