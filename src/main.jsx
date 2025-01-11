97540c1cffee77438ec4713f19518ff7895a8a77
nadrojddik <mail@jordankidd.com>
2025-01-10 19:49:04 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,3 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+
+console.log('main.jsx loading...')
+console.log('React version:', React.version)
+console.log('Environment variables:', import.meta.env)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
