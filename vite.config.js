205fca780028b81c8f1387c4f03233c9a296c57c
nadrojddik <mail@jordankidd.com>
2025-01-10 22:43:35 -0500

    vite config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: '/restaurant-finder/',
-  plugins: [react()]
+  plugins: [react()],
+  define: {
+    'process.env.VITE_GOOGLE_MAPS_API_KEY': JSON.stringify(process.env.VITE_GOOGLE_MAPS_API_KEY)
+  }
 })
